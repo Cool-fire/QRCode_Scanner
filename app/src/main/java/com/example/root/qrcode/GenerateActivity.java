5558f0001466d6ffc580f5e3fbb7fb95448a8e59
Coo-fire <upendrareddy2511@gmail.com>
2018-04-08 20:50:30 +0530

    revoked

--- app/src/main/java/com/example/root/qrcode/GenerateActivity.java
+++ app/src/main/java/com/example/root/qrcode/GenerateActivity.java
@@ -34,14 +34,7 @@ public class GenerateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         editField = (EditText)findViewById(R.id.writeID);
         generateBttn = (Button)findViewById(R.id.but);
